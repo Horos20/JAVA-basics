dc5aa8693a8cb4bec89892c2a13ce10554240048
Matis Rammi <themrrammi@gmail.com>
2022-09-02 15:13:54 +0300

    Week 1 - Ex5

---
+++ week1/Ex5.java
@@ -0,0 +1,14 @@
+package week1;
+
+public class Ex5 {
+    public static void main(String[] args) {
+        Integer days = 365;
+        Integer hours = 24;
+        Integer minutes = 60;
+        Integer seconds = 60;
+
+        Integer secondsInYear = days * hours * minutes * seconds;
+
+        System.out.println("There are " + secondsInYear + " seconds in a year.");
+    }
+}
