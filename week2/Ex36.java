8c7fbb4a5bd7396bf9e2dad1865b4b3580d19be6
Matis Rammi <themrrammi@gmail.com>
2022-09-08 12:35:09 +0300

    Week 2 - Ex36.4: Counting the average

--- week2/Ex36.java
+++ week2/Ex36.java
@@ -20,5 +20,6 @@ public class Ex36 {
         System.out.println("Thank you and see you later!");
         System.out.println("The sum is " + sum);
         System.out.println("How many numbers " + count);
+        System.out.println("Average " + (double)sum / count);
     }
 }
