e90b2a6224878e63945c4be2b11f6d5948ea0170
Matis Rammi <themrrammi@gmail.com>
2022-09-08 13:55:56 +0300

    Week 2 - Ex41.3: Counting the guesses

--- week2/Ex41.java
+++ week2/Ex41.java
@@ -7,14 +7,16 @@ public class Ex41 {
     public static void main(String[] args) {
         Random random = new Random();
         int randomNumber = random.nextInt(100 - 0 + 1) + 0;
+        int count = 0;
         Scanner reader = new Scanner(System.in);
         System.out.println("Guess a number: ");
         while (true) {
             int numberGuessed = Integer.parseInt(reader.nextLine());
+            count++;
             if (numberGuessed > randomNumber) {
-                System.out.println("The number is lesser");
+                System.out.println("The number is lesser, guesses made: " + count);
             } else if (numberGuessed < randomNumber) {
-                System.out.println("The number is greater");
+                System.out.println("The number is greater, guesses made: " + count);
             } else {
                 System.out.println("Congratulations, your guess is correct!");
                 break;
