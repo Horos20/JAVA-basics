85746291ac7e231f34071b37739261b17825c98c
Matis Rammi <themrrammi@gmail.com>
2022-09-09 11:46:30 +0300

    Week 3 - Ex64

---
+++ week3/Ex64.java
@@ -0,0 +1,21 @@
+package week3;
+import java.util.ArrayList;
+
+public class Ex64 {
+    public static double average(ArrayList<Integer> list) {
+        double sum = 0;
+        for(Integer d : list)
+            sum += d;
+        return (sum / list.size());
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        list.add(3);
+        list.add(2);
+        list.add(7);
+        list.add(2);
+
+        System.out.println("The average is: " + average(list));
+    }
+}
