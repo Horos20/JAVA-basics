889f978a0e9c6e939a5994ee9abfb0d15c5fe7bb
Matis Rammi <themrrammi@gmail.com>
2022-09-09 11:02:35 +0300

    Week 2 - Ex79.2: sum and average

--- week4/NumberStatistics.java
+++ week4/NumberStatistics.java
@@ -2,16 +2,26 @@ package week4;
 
 public class NumberStatistics {
     private int amountOfNumbers;
+    private int sum;
 
     public NumberStatistics() {
         this.amountOfNumbers = 0;
     }
 
     public void addNumber(int number) {
+        sum += number;
         amountOfNumbers++;
     }
 
     public int amountOfNumbers() {
         return amountOfNumbers;
+    }
+
+    public int sum() {
+        return sum;
+    }
+
+    public double average() {
+        return sum / (double)amountOfNumbers;
     }
 }
