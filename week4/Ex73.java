00343de0ff3fc2b045e2b01683bc9775cd3494bf
Matis Rammi <themrrammi@gmail.com>
2022-09-09 09:48:02 +0300

    Week 4 - Ex73

---
+++ week4/Ex73.java
@@ -0,0 +1,8 @@
+package week4;
+
+public class Ex73 {
+    public static void main(String[] args) {
+        Product productA = new Product("Banana", 1.1, 13);
+        productA.printProduct();
+    }
+}
