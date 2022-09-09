81f6f95d3dd1ceb04f14bae1adcd9448cd36dcc7
Matis Rammi <themrrammi@gmail.com>
2022-09-09 10:23:54 +0300

    Week 2 - Ex46

---
+++ week2/Ex46.java
@@ -0,0 +1,14 @@
+package week2;
+
+public class Ex46 {
+    public static double average(int number1, int number2, int number3, int number4) {
+        int sum = number1 + number2 + number3 + number4;
+        return sum / (double)4;
+    }
+
+    public static void main(String[] args) {
+        double answer = average(4, 3, 6, 1);
+        System.out.println("average: " + answer);
+    }
+
+}
