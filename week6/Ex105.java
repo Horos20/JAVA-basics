d37abc2a46a7ab8596026d0fe4dc80ec2b4c3acf
Matis Rammi <themrrammi@gmail.com>
2022-09-16 12:22:37 +0300

    Week 6 - Ex105.1: Is greater than

---
+++ week6/Ex105.java
@@ -0,0 +1,9 @@
+package week6;
+
+public class Ex105 {
+    public static void main(String[] args) {
+        GuessingGame game = new GuessingGame();
+
+        System.out.println(game.isGreaterThan(32));
+    }
+}
