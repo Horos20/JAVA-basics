446e560a13d00ae4e090a1793ee56aaa52d68fb4
Matis Rammi <themrrammi@gmail.com>
2022-09-16 13:39:39 +0300

    Week 7 - Ex7.1: Class Dictionary

---
+++ week7/Dictionary.java
@@ -0,0 +1,13 @@
+package week7;
+
+import java.util.HashMap;
+
+public class Dictionary {
+    HashMap<String, String> words = new HashMap<String, String>();
+    public String translate(String word) {
+        return words.get(word);
+    }
+    public void add(String word, String translation) {
+        words.put(word, translation);
+    }
+}
