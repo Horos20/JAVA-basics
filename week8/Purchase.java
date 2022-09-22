e9af4ad87efd94be1af86faf0d84811c525a12c5
Matis Rammi <themrrammi@gmail.com>
2022-09-22 09:39:05 +0300

    Week 8 - Ex12.7: Only One Purchase Object for One Product

--- week8/Purchase.java
+++ week8/Purchase.java
@@ -21,9 +21,6 @@ public class Purchase {
 
     @Override
     public String toString() {
-        return "Purchase{" +
-                "product='" + product + '\'' +
-                ", amount=" + amount +
-                '}';
+        return product + ": " + amount;
     }
 }
